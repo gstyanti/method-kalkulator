91ecc6919e5f71da509483c1a35b7190b83559eb
gstyanti <101311934+gstyanti@users.noreply.github.com>
2022-04-05 22:26:57 +0800

    Add files via upload

---
+++ Numbers.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public  class Numbers
+    {
+        private float result;
+
+        public int FindMinimum(int number1, int number2)
+        {
+            /* local variabel declaration */
+            int result;
+
+            if (number1 < number2)
+                result = number1;
+            else
+                result = number2;
+            return result;
+        }
+        
+        public int FindMinimum(int number1, int number2, float number3) 
+        { 
+
+            if (number1 < number2 && number1 < number3) 
+                result = number1;
+             else if (number2 < number3) 
+                result = number2;
+            else 
+                result = number3;
+                return  result; 
+        }
+        public int FindMaximum(int number1, int number2)
+        {
+            /* local variabel declaration */
+            int result;
+
+            if (number1 > number2)
+                result = number1;
+            else
+                result = number2;
+            return result;
+        }
+        public int FindMakximum(int number1, int number2, float number3)
+        {
+
+            if (number1 > number2 && number1 > number3)
+                result = number1;
+            else if (number2 > number3)
+                result = number2;
+            else
+                result = number3;
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers number = new Numbers();
+
+            Console.WriteLine("Minimum #1: {0}", number.FindMinimum(5, 3));
+            Console.WriteLine("Minimum #2: {0}", number.FindMinimum(3, 2, 4));
+            Console.WriteLine(); //tambahkan baris kosong
+            Console.WriteLine("Maximum #1: {0}", number.FindMaximum(5, 3));
+            Console.WriteLine("Maximum #2: {0}", number.FindMaximum(3, 2, 4));
+
+        }
+            
+    }
+}
